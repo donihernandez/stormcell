5a5af39a05c83963b5ff49d667d17a6129d296ef
Doni <donihernandez003@gmail.com>
2021-05-20 11:45:44 -0400

    Migraciones y modelos

---
+++ database/migrations/2021_04_28_165532_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->string('size');
+            $table->float('price');
+            $table->float('discount');
+            $table->string('type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2021_04_28_192843_create_pictures_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->foreignId('image_id')->constrained();
+            $table->integer('picture_id');
+            $table->string('picture_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
